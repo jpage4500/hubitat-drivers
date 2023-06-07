0114ff417391b818cecdf5c6ccde813a636b1db4
Joe Page <jpage4500@gmail.com>
2023-06-07 18:48:10 -0400

    - don't set avatar when not set

--- life360/life360_driver.groovy
+++ life360/life360_driver.groovy
@@ -181,6 +181,10 @@ def updated() {
     refresh()
 }
 
+/**
+ * called from parent app with member location
+ * @return true if member's location changed (lat/lng/acc) from last update
+ */
 Boolean generatePresenceEvent(member, thePlaces, home) {
     if (member.location == null) {
         // log.info "Life360+ Driver: no location set for $member"
@@ -249,12 +253,13 @@ Boolean generatePresenceEvent(member, thePlaces, home) {
     def String avatarHtml
     if (member.avatar != null){
         avatar = member.avatar
+        sendEvent( name: "avatar", value: avatar )
         avatarHtml =  "<img src= \"${avatar}\">"
     } else {
-        avatar = "not set"
+        // only necessary to clear out the "not set" state
+        device.deleteCurrentState("avatar")
         avatarHtml = "not set"
     }
-    sendEvent( name: "avatar", value: avatar )
 
     // *** Location ***
     def Double distanceAway = haversine(latitude, longitude, homeLatitude, homeLongitude) * 1000 // in meters
@@ -356,13 +361,12 @@ Boolean generatePresenceEvent(member, thePlaces, home) {
 
     // ** HTML attributes (optional) **
     if (generateHtml) {
-        // send HTML avatar if generateHTML is enabled; otherwise clear it (only if previously set)
         sendEvent( name: "avatarHtml", value: avatarHtml)
 
-        def String binTransita
-        if (isDriving) binTransita = "Driving"
-        else if (inTransit) binTransita = "Moving"
-        else binTransita = "Not Moving"
+        def String transitDesc
+        if (isDriving) transitDesc = "Driving"
+        else if (inTransit) transitDesc = "Moving"
+        else transitDesc = "Not Moving"
 
         int sEpoch = device.currentValue('since')
         if(sEpoch == null) {
@@ -385,7 +389,7 @@ Boolean generatePresenceEvent(member, thePlaces, home) {
         tileMap += "At: <a href='${theMap}' target='_blank'>${address1 == "No Data" ? "Between Places" :address1}</a><br>"
         tileMap += "Since: ${dateSince}<br>"
         tileMap += (sStatus == "At Home") ? "" : "${sStatus}<br>"
-        tileMap += "${binTransita}"
+        tileMap += "${transitDesc}"
         if(address1 == "No Data" ? "Between Places" : address1 != "Home" && inTransit) {
             tileMap += " @ ${sprintf("%.1f", speed)} "
             tileMap += isMiles ? "MPH":"KPH"
