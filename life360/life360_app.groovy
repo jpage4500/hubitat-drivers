7ff9d94215eafb8a650660ab3ade43ff69531c97
Matt <matt.palermo@gmail.com>
2024-12-22 11:00:07 -0500

    missing changelog entry

--- life360/life360_app.groovy
+++ life360/life360_app.groovy
@@ -13,6 +13,7 @@ import java.net.http.HttpTimeoutException
  * - see discussion: https://community.hubitat.com/t/release-life360/118544
  *
  * Changes:
+ *  5.0.11 - 12/22/24 - bugfix when polling > 1 min
  *  5.0.10 - 12/21/24 - add some randomness
  *  5.0.9 - 12/19/24 - try a different API when hitting 403 error
  *  5.0.8 - 12/18/24 - added cookies found by @user3774
